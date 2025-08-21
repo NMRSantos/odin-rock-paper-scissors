820a8d8ea9eeef13212597cb819b9a6e5d1cb615
NMRSantos <170880035+NMRSantos@users.noreply.github.com>
2025-08-22 00:13:40 +0100

    Add barebones dynamic UI and logic

--- scripts.js
+++ scripts.js
@@ -22,36 +22,69 @@ function getHumanChoice() {
         }
 }
 
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
-    function playRound(humanChoice, computerChoice) {
-        if (computerChoice === humanChoice) {
-            console.log(`Your Score: ${humanScore}`);
-            console.log(`Computer Score:${computerScore}`);
-            console.log(`It's a draw! You both picked ${humanChoice}!`);
-        }
-        else if (
-                (computerChoice === `Rock` && humanChoice === `Scissors`) ||
-                (computerChoice === `Paper` && humanChoice === `Rock`) ||
-                (computerChoice === `Scissors` && humanChoice === `Paper`)
-            ) {
-            computerScore++;
-            console.log(`Your Score: ${humanScore}`);
-            console.log(`Computer Score ${computerScore}`);
-            console.log(`You lost! ${computerChoice} beats ${humanChoice}!`);
-        }
-        else {
-            humanScore++;
-            console.log(`Your Score: ${humanScore}`);
-            console.log(`Computer Score ${computerScore}`);
-            console.log(`You won! ${humanChoice} beats ${computerChoice}!`);
-        }
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    if (computerChoice === humanChoice) {
+        console.log(`Your Score: ${humanScore}`);
+        console.log(`Computer Score:${computerScore}`);
+        humanTally.textContent = `Your Score: ${humanScore}`;
+        computerTally.textContent = `Your Score: ${computerScore}`;
+        console.log(`It's a draw! You both picked ${humanChoice}!`);
     }
-    playRound(getHumanChoice(), getComputerChoice());
-    playRound(getHumanChoice(), getComputerChoice());
-    playRound(getHumanChoice(), getComputerChoice());
-    playRound(getHumanChoice(), getComputerChoice());
-    playRound(getHumanChoice(), getComputerChoice());
+    else if (
+        (computerChoice === `Rock` && humanChoice === `Scissors`) ||
+            (computerChoice === `Paper` && humanChoice === `Rock`) ||
+            (computerChoice === `Scissors` && humanChoice === `Paper`)
+        ) {
+        computerScore++;
+        console.log(`Your Score: ${humanScore}`);
+        console.log(`Computer Score ${computerScore}`);
+        humanTally.textContent = `Your Score: ${humanScore}`;
+        computerTally.textContent = `Your Score: ${computerScore}`;
+        console.log(`You lost! ${computerChoice} beats ${humanChoice}!`);
+    }
+    else {
+        humanScore++;
+        console.log(`Your Score: ${humanScore}`);
+        console.log(`Computer Score ${computerScore}`);
+        humanTally.textContent = `Your Score: ${humanScore}`;
+        computerTally.textContent = `Your Score: ${computerScore}`;
+        console.log(`You won! ${humanChoice} beats ${computerChoice}!`);
+    }
 }
+
+function playGame() {
+
+};
+
+const rockButton = document.querySelector(".rockButton");
+const paperButton = document.querySelector(".paperButton");
+const scissorsButton = document.querySelector(".scissorsButton");
+
+rockButton.addEventListener("click", () => {
+    let humanChoice = "Rock";
+    playRound(humanChoice, getComputerChoice())
+});
+
+paperButton.addEventListener("click", () => {
+    let humanChoice = "Paper";
+    playRound(humanChoice, getComputerChoice())
+});
+
+scissorsButton.addEventListener("click", () => {
+    let humanChoice = "Scissors";
+    playRound(humanChoice, getComputerChoice())
+});
+  
+const div = document.createElement("div");
+const script = document.querySelector("script");
+const body = document.querySelector("body");
+body.appendChild(div);
+
+let humanTally = document.createElement("p");
+div.append(humanTally);
+let computerTally = document.createElement("p");
+div.append(computerTally);
 
